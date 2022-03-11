c4bdb3c26f77c7414f6d4ba5ec896f96526a35dd
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-03-11 20:32:15 +0530

    rectify some code in operate function

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ let decimal = document.querySelector(".decimal");
 let del = document.querySelector(".del");
 let displayScreen = document.getElementById("screen");
 
-let number1 = "0";
-let number2 = "0";
+let number1 = "";
+let number2 = "";
 let sign = "";
 let isNum1Filled = false;
 let isNum2Filled = false;
-let result = "0"
+let result = ""
 
 function handleNumber() {
     if (isNum1Filled == false) {
@@ -29,25 +29,30 @@ function handleOperator() {
     isNum1Filled = true;
 }
 
-function operate(num1, num2) {
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
+function operate() {
+    number1 = parseFloat(number1);
+    number2 = parseFloat(number2);
+    // console.log(number1, number2);
     if (sign == "/") {
-        result = num1 / num2;
+        result = number1 / number2;
     } else if (sign == "*") {
-        result = num1 * num2;
+        result = number1 * number2;
     } else if (sign == "-") {
-        result = num1 - num2;
+        result = number1 - number2;
     } else if (sign == "+"){
-        result = num1 + num2;
+        result = number1 + number2;
     } else if (sign == "%") {
-        result = num1 % num2;
+        result = number1 % number2;
     } else {
-        result = num1; //handle later
+        result = number1; //handle later
     }
     console.log(result);
     // result = String(result);
     displayScreen.value = result;
+    number1 = result;
+    number2 = "";
+    isNum1Filled = true;
+    isNum2Filled = false;
 }
 
 for (let i = 0; i < numberBtns.length; i++) {
@@ -57,4 +62,4 @@ for (let operator of operators) {
     operator.addEventListener("click", handleOperator);
 }
 
-equal.addEventListener("click", ()=> operate(number1, number2));
+equal.addEventListener("click", operate);

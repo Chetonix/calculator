59030bba2d3f0eec6480b6f1d8145ebe4e659355
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-03-11 19:50:18 +0530

    add incomplete opeartor code to index.js

--- index.js
+++ index.js
@@ -7,19 +7,27 @@ let del = document.querySelector(".del");
 
 let number1 = "0";
 let number2 = "0";
+let sign = "";
 let isNum1Filled = false;
 let isNum2Filled = false;
-function addNumber() {
+function handleNumber() {
     if (isNum1Filled == false) {
         number1 = number1 + this.textContent;
-        // console.log(number1);
+        console.log(number1);
     }
 }
-
+function handleOperator() {
+    sign = this.textContent;
+    console.log(sign);
+}
 
 
 
 
 for (let i = 0; i < numberBtns.length; i++) {
-    numberBtns[i].addEventListener("click", addNumber);
+    numberBtns[i].addEventListener("click", handleNumber);
 }
+for (let operator of operators) {
+    operator.addEventListener("click", handleOperator);
+}
+
